302ab5c657ce2b5e02f68d0099953b8f728ea9bc
jekyenin <jekyenin@yandex.ru>
2014-06-26 17:57:23 +0300

    script was tested

--- letterify.js
+++ letterify.js
@@ -1,11 +1,17 @@
 function letterify(node){
 	var letters;
 	if(node.length>1){
-		for(var i=0,length=node.length;i<length,i++){
+		for(var i=0,length=node.length;i<length;i++){
 			letterify(node[i]);
 		}
 	}
+	else if(node.length==undefined){
+		letToSpan(node)
+	}
 	else if(node.length==1){
+		letToSpan(node[0])
+	}
+	function letToSpan(node){
 		letters=node.textContent;
 		node.innerHTML='';
 		for(var i=0,length=letters.length;i<length;i++){
@@ -14,8 +20,4 @@ function letterify(node){
 			node.appendChild(span);
 		}
 	}
-	else(node.length<1){
-		console.log('Error no elem to proccess');
-	}
-
 }
